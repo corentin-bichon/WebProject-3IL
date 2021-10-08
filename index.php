7bc5f8e90d3024876c52ab620f34b52dd0508d7a
Corentin Bichon <bichon.corentin@gmail.com>
2021-10-08 09:22:58 +0200

    update index

--- index.php
+++ index.php
@@ -8,18 +8,6 @@
 	</head>
 
 	<body>
-			<div class=logoMenu>
-			<img src="ressources/logo/logo_small.png" alt="Logo_RuthenQuiz" title="Logo_RuthenQuiz" id="logo"/>
-
-		</div>
-
-		<div class="navbar">
-       		 <a href="index.html">Accueil</a> 
-       		 <a href="forms.html">Formulaire</a>
-       		 <a href="pages/theme.html">Thèmes</a>
-       		 <a href="create.html">Créer</a> 
-        	 <a href="account.html">Compte</a> 
-		</div>
 
 		<?php require 'extensions/navbar.php'; ?>
 
