df2b867e0073ae5bb3286307cc19b0200f219240
Corentin Bichon <bichon.corentin@gmail.com>
2021-10-08 09:06:32 +0200

    adding footer en php files

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
 <html> 
+<html lang="fr">
 	<head> 
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="CSS\mainSheet.css" type="text/css"/>
 		<link rel="shortcut icon" type="image/x-icon" href="ressources/logo/favicon.ico">
 		<title>Accueil - Ruthen Quiz</title>
@@ -8,18 +9,7 @@
 
 	<body>
 
-		<div class=logoMenu>
-			<img src="ressources/logo/logo_small.png" alt="Logo_RuthenQuiz" title="Logo_RuthenQuiz" id="logo"/>
-
-		</div>
-
-		<div class="navbar">
-       		 <a href="index.html">Accueil</a> 
-       		 <a href="forms.html">Formulaire</a>
-       		 <a href="Pages/Theme.html">Thèmes</a>
-       		 <a href="create.html">Créer</a> 
-        	 <a href="account.html">Compte</a> 
-		</div>
+		<?php require 'extensions/navbar.php'; ?>
 
 		<br><br><br><br><br>
 
@@ -40,8 +30,12 @@
 			<div> 
 				Présentation du site de Quiz
 			</div>
-		</div>
 
+		</section>
 
+		<br><br><br><br><br>
 	</body>
+
+	<?php require 'extensions/footer.php'; ?>
+
 </html>
