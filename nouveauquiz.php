5497b162c81033b29a5f64f83d7dbe75b152560f
Corentin Bichon <bichon.corentin@gmail.com>
2021-10-29 11:59:31 +0200

    Création de la page Création de Quiz, il manque la vrai upload de la photo et trouver la bonne réponse

--- nouveauquiz.php
+++ nouveauquiz.php
@@ -13,7 +13,7 @@
 
 		<section>
 	
-			<div class="cadre">
+			<div class="cadre-nouveauquiz">
 
 				<div class="logo-arrondi"> </div>
 
@@ -21,37 +21,44 @@
 					<span> Créer un nouveau Quiz </span>
 				</div>
 
-				<form action="extensions/creationquiz.php" method="POST">
+				<form action="extensions/creationquiz.php" class="formulaire-nouveauquiz" method="POST">
 
-                    <div class="connexion-identifiant">
-                    	<img src="ressources/icon/utilisateur.png" id="connexion-icon" />
-                    	<input type="text" name="prenom" placeholder="Prenom" required>
+                    <div class="information-nouveauquiz">
+                         <div class="entree-nouveauquiz nom-nouveauquiz">
+                         	<input type="text" name="nomquiz" placeholder="nomquiz" required>
+                         </div>
+
+                         <div class="entree-nouveauquiz image-nouveauquiz">
+                              <input type="file" name="image" src="ressources/icon/ajouterImage.png" id="imageicon-nouveau">
+                          </div>
                     </div>
 
-					<div class="connexion-identifiant">
-						<img src="ressources/icon/utilisateur.png" id="connexion-icon" />
-						<input type="text" name="nom" placeholder="Nom" required>
-					</div>
+                <?php for ( $i = 1; $i <= 2; $i++ ) {
+                    echo '<div class="ligne-nouveauquiz">
+					    <div class="entree-nouveauquiz question-nouveauquiz">
+					    	<input type="text" name="question'.$i.'" placeholder="question" required>
+					    </div>
 
-					<div class="connexion-identifiant">
-						<img src="ressources/icon/utilisateur.png" id="connexion-icon" />
-						<input type="text" name="username" placeholder="pseudo" required>
-					</div>
-	
-					<div class="connexion-identifiant">
-					    <img src="ressources/icon/cadena.png" id="connexion-icon" />
-						<input type="password" name="password" placeholder="Mot de passe" required>
-					</div>
+					    <div class="entree-nouveauquiz reponse-nouveauquiz">
+					        <input type="radio" id="reponseA" name="reponse'.$i.'" value="reponseA" checked>
+                        	<input type="text" name="reponseA'.$i.'" placeholder="reponseA" required>
+                        </div>
 
-					<div class="connexion-identifiant">
-                    	<img src="ressources/icon/cadena.png" id="connexion-icon" />
-                    	<input type="password" name="password2" placeholder="Confirmer le mot de passe" required>
-                    </div>
+                        <div class="entree-nouveauquiz reponse-nouveauquiz">
+                        	<input type="radio" id="reponseB" name="reponse'.$i.'" value="reponseB">
+                            <input type="text" name="reponseB'.$i.'" placeholder="reponseB" required>
+                        </div>
+
+                        <div class="entree-nouveauquiz reponse-nouveauquiz">
+                        	 <img src="ressources/icon/information.png" id="imageicon-nouveau" />
+                             <input type="text" name="inforeponse'.$i.'" placeholder="Supplément reponse">
+                        </div>
+                    </div>' ;
+				} ?>
 
 					<div class=quiz-valider>
 						<input type="submit" id='valider' value='Mettre en ligne le quiz' >
 					</div>
-
 				</form>
 			</div>
 	
